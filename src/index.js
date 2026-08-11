57a4fcf916111fd9b15a100296e0142294b45180
imanrao90 <imanrao90@gmail.com>
2026-08-11 20:33:00 +0500

    User & Video models added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import dotenv from "dotenv"
 import { connectDB } from "./db/index.js"
+import { app } from "./app.js"
+
+app.get('/', () => {
+
+})
 
 dotenv.config({path: './.env'})
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`Server running on port: http://localhost:${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log('Mongodb connection failed!', err)
+})
 
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,12 @@
+export class ApiResponse extends Error{
+    constructor(
+        statusCode,
+        data,
+        message= "Success"
+    ){
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.success = statusCode < 400
+    }
+}
